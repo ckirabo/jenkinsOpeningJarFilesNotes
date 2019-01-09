b9228031c4160a4266a48b24bdb11a9b94713982
clare <kirabo646@googlemail.com>
2019-01-09 14:59:17 +0000

    file used as notes that contains info on how to chnage the ports

---
+++ changingPortTemplate.bash
@@ -0,0 +1,8 @@
+#running a java package springboot application after installing maven in virutal machine changin port in github
+#server.port = 9000 in /src/main/resources/application.properties
+#enter file and then running hello world example.
+./src/main
+cd target
+ls
+./hello-world-0.0.1-SNAPSHOT.jar
+java -jar hello-world-0.0.1-SNAPSHOT.jar
